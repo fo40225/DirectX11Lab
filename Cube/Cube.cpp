31f2e6434cd8dd5e991747097557f143bd6db401
fo40225 <fo40225@hotmail.com>
2016-03-09 00:02:30 +0800

    add cube WinMain entry point

---
+++ Cube/Cube.cpp
@@ -0,0 +1,14 @@
+// Cube.cpp : 定義應用程式的進入點。
+//
+
+#include "stdafx.h"
+#include "Cube.h"
+
+int APIENTRY wWinMain(
+	_In_ HINSTANCE hInstance,
+	_In_opt_ HINSTANCE hPrevInstance,
+	_In_ LPWSTR    lpCmdLine,
+	_In_ int       nCmdShow)
+{
+	return 0;
+}
