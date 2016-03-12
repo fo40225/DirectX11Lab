dedca95df41af4c2d05da78ad3870c7c4527ff02
fo40225 <fo40225@hotmail.com>
2016-03-12 14:11:38 +0800

    create DirectX11Lab project

---
+++ DirectX11Lab/DirectX11Lab.cpp
@@ -0,0 +1,15 @@
+// DirectX11Lab.cpp : 定義應用程式的進入點。
+//
+
+#include "stdafx.h"
+#include "DirectX11Lab.h"
+
+int APIENTRY wWinMain(
+	_In_     HINSTANCE hInstance,
+	_In_opt_ HINSTANCE hPrevInstance,
+	_In_     LPWSTR    lpCmdLine,
+	_In_     int       nCmdShow
+	)
+{
+	return 0;
+}
