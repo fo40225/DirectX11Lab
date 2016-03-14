c038419cadc214ca22bfe95da81e0d0652351a46
fo40225 <fo40225@hotmail.com>
2016-03-14 23:01:56 +0800

    Tutorial 00: Win32 Basics https://msdn.microsoft.com/en-us/library/windows/apps/ee416435.aspx

--- DirectX11Lab/DirectX11Lab.cpp
+++ DirectX11Lab/DirectX11Lab.cpp
@@ -11,5 +11,78 @@ int APIENTRY wWinMain(
 	_In_     int       nCmdShow
 	)
 {
+	UNREFERENCED_PARAMETER(hPrevInstance);
+	UNREFERENCED_PARAMETER(lpCmdLine);
+
+	if (FAILED(InitWindow(hInstance, nCmdShow)))
+		return 0;
+
+	//
+	// Main message loop
+	//
+	MSG msg = { 0 };
+	while (GetMessage(&msg, NULL, 0, 0))
+	{
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+
+	return (int)msg.wParam;
+}
+
+HRESULT InitWindow(HINSTANCE hInstance, int nCmdShow)
+{
+	//
+	// Register class
+	//
+	WNDCLASSEX wcex = {};
+	wcex.cbSize = sizeof(WNDCLASSEX);
+	wcex.style = CS_HREDRAW | CS_VREDRAW;
+	wcex.lpfnWndProc = WndProc;
+	wcex.cbClsExtra = 0;
+	wcex.cbWndExtra = 0;
+	wcex.hInstance = hInstance;
+	wcex.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wcex.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+	wcex.lpszMenuName = NULL;
+	wcex.lpszClassName = L"TutorialWindowClass";
+	if (!RegisterClassEx(&wcex))
+		return FALSE;
+
+	//
+	// Create window
+	//
+	g_hInst = hInstance; // Store instance handle in our global variable
+	RECT rc = { 0, 0, 640, 480 };
+	AdjustWindowRect(&rc, WS_OVERLAPPEDWINDOW, FALSE);
+	g_hWnd = CreateWindow(L"TutorialWindowClass", L"Direct3D 10 Tutorial 0: Setting Up Window",
+		WS_OVERLAPPEDWINDOW,
+		CW_USEDEFAULT, CW_USEDEFAULT, rc.right - rc.left, rc.bottom - rc.top, NULL, NULL, hInstance,
+		NULL);
+	if (!g_hWnd)
+		return FALSE;
+
+	ShowWindow(g_hWnd, nCmdShow);
+
+	return S_OK;
+}
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	PAINTSTRUCT ps;
+	HDC hdc;
+	switch (message)
+	{
+	case WM_PAINT:
+		hdc = BeginPaint(hWnd, &ps);
+		EndPaint(hWnd, &ps);
+		break;
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		break;
+	default:
+		return DefWindowProc(hWnd, message, wParam, lParam);
+	}
+
 	return 0;
 }
--- DirectX11Lab/DirectX11Lab.h
+++ DirectX11Lab/DirectX11Lab.h
@@ -1,1 +1,7 @@
 #pragma once
+
+HINSTANCE g_hInst = nullptr;
+HWND      g_hWnd  = nullptr;
+
+HRESULT InitWindow(HINSTANCE hInstance, int nCmdShow);
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam);
